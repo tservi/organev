2099765f1fc5196fe8a1565a387befc2a8b12a5c
Jean Tinguely Awais <jeantinguelyawais@gmail.com>
2019-12-31 16:20:51 +0100

    Update organev.php

--- organev.php
+++ organev.php
@@ -1,11 +1,13 @@
 <?php
 /**
 * Plugin Name: Organev
-*
+* Plugin URI: http://mon-blog.fr/plugins/ 
+* Description: Ce plugin wordpress permet de créer des evénements
+* Version: 0.1 
+* Author: Jean Tinguely Awais aka t-servi.com 
+* Author URI: http://www.t-servi.com
+* License: Ma licence GPL2 
 *
 *
 */
-
-
-
 
